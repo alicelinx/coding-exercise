4b6f49f658f6f023f140e6a19c98c475cd7ee6aa
Alice Lin <alicexxlin@gmail.com>
2024-01-04 01:23:26 +0800

    install and implement Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Flex } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex
+      color="red"
+      justifyContent="center"
+    >Hello World</Flex>
   );
 }
 
