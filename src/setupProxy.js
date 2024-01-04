11790463d2a5bc5762f5221fd275d8703e6895e9
Alice Lin <alicexxlin@gmail.com>
2024-01-04 19:20:59 +0800

    refactor code and remove path rewriting

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+const proxy = {
+  target: 'https://storage.googleapis.com/openhouse-ai-fe-coding-test',
+  changeOrigin: true
+};
+
 module.exports = function(app) {
   app.use(
     '/api',
-    createProxyMiddleware({
-      target: 'https://storage.googleapis.com/openhouse-ai-fe-coding-test',
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': '',
-      },
-    })
+    createProxyMiddleware(proxy)
   );
 };
